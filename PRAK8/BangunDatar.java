2a23bea6933f0b4f40badb03d17a917f96cded15
falihdwnd <falihdewanda@gmail.com>
2025-05-31 21:49:36 +0700

    Add files via upload

---
+++ PRAK8/BangunDatar.java
@@ -0,0 +1,5 @@
+package PRAK8;
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
---
+++ PRAK8/Lingkaran.java
@@ -0,0 +1,13 @@
+package PRAK8;
+
+public class Lingakaran extends BangunDatar{
+    private double jejari;
+
+    public Lingakaran(double jejari){
+        this.jejari = jejari;
+    }
+
+    public double hitungKeliling(){
+        return 2*jejari*3.14;
+    }
+}
